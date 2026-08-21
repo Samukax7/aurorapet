fdc3d95a5e801e40027de699bef66dc90707be91
Samukax7 <1.15289961e+08+Samukax7@users.noreply.github.com>
2026-08-21 06:59:10 +0000

    Publicar BattleStage visível na batalha

--- docs/index.service.worker.js
+++ docs/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1787294029|5541967';
+const CACHE_VERSION = '1787295519|4085653';
 /** @type {string} */
 const CACHE_PREFIX = 'AuroraPet V 0.0-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
