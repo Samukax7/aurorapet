601c91d2fffaad3b659f68acd68ba3b9387183bc
Samukax7 <1.15289961e+08+Samukax7@users.noreply.github.com>
2026-08-21 08:18:38 +0000

    Publicar layout compacto da batalha

--- docs/index.service.worker.js
+++ docs/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1787298035|4377682';
+const CACHE_VERSION = '1787300297|4286659';
 /** @type {string} */
 const CACHE_PREFIX = 'AuroraPet V 0.0-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
