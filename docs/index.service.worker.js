6d1f38205adea3e61d19caa0e24d633bd49bfd58
Samukax7 <1.15289961e+08+Samukax7@users.noreply.github.com>
2026-08-21 02:12:16 +0000

    Atualizar build Web V 0.0

--- docs/index.service.worker.js
+++ docs/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1787271956|4161417';
+const CACHE_VERSION = '1787278299|6121868';
 /** @type {string} */
 const CACHE_PREFIX = 'AuroraPet V 0.0-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
