990dac4fef2a303850e86ca8d625020acac18c3e
Samukax7 <1.15289961e+08+Samukax7@users.noreply.github.com>
2026-08-22 15:33:32 +0000

    Replace lobby theme with user track

--- docs/index.service.worker.js
+++ docs/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1787399324|6576330';
+const CACHE_VERSION = '1787410181|4781682';
 /** @type {string} */
 const CACHE_PREFIX = 'AuroraPet V 0.1-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
