e230c6f12a290a6db3c75d2461605522e148a527
Samukax7 <1.15289961e+08+Samukax7@users.noreply.github.com>
2026-08-26 21:44:15 +0000

    fix: include startup dependencies in web export

--- docs/index.service.worker.js
+++ docs/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1787779022|100393648';
+const CACHE_VERSION = '1787780499|83744635';
 /** @type {string} */
 const CACHE_PREFIX = 'AuroraPet V 0.1-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
