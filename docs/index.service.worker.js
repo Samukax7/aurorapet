d0fbe4c66ace26334dfd6e7e388ca02660ca03c3
Samukax7 <1.15289961e+08+Samukax7@users.noreply.github.com>
2026-08-27 13:35:23 +0000

    build: publish readable UI web export

--- docs/index.service.worker.js
+++ docs/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1787788557|91857440';
+const CACHE_VERSION = '1787837431|70416681';
 /** @type {string} */
 const CACHE_PREFIX = 'AuroraPet V 0.1-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
