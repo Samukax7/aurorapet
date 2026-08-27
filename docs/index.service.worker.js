e10f0231509c24dc564e04c9c39395fa2036895b
Samukax7 <1.15289961e+08+Samukax7@users.noreply.github.com>
2026-08-27 16:15:52 +0000

    fix: invalidate web cache for refreshed icons

--- docs/index.service.worker.js
+++ docs/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1787844086|238870066';
+const CACHE_VERSION = 'aurorapet-icons-refresh-20260827-v1';
 /** @type {string} */
 const CACHE_PREFIX = 'AuroraPet V 0.1-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
