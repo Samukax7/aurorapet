7b061e4feed4581c1c962c31815294171de72b18
SAMUKASX7 <115289961+Samukax7@users.noreply.github.com>
2026-08-29 12:26:57 -0300

    fix: cap oversized animation textures for mobile web

--- docs/mobile/index.service.worker.js
+++ docs/mobile/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1788015531|146068392';
+const CACHE_VERSION = '1788017111|75271375';
 /** @type {string} */
 const CACHE_PREFIX = 'AuroraPet mobile-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
